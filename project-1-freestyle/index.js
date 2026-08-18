6464a4616c162c3e3b9377907c7ddc92e228258f
MOHAMED ISMAIL KS <mohamed.ismail.cloud@gmail.com>
2026-08-18 07:43:26 +0530

    Update index.js

--- project-1-freestyle/index.js
+++ project-1-freestyle/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.status(200).json({
     status: "ok",
-    project: "project-1-freestyles",
+    project: "project-1-freestyless",
     message: "Welcome to Project 1 API",
   });
 });
