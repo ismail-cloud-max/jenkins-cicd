47f5809b43b4b81ff5cc4077cb7fb2a861dd4aef
MOHAMED ISMAIL KS <mohamed.ismail.cloud@gmail.com>
2026-08-19 21:06:07 +0530

    Update index.js

--- project-2-pipeline-jenkinsfile/index.js
+++ project-2-pipeline-jenkinsfile/index.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
   res.status(200).json({
     status: "ok",
     project: "project-2-pipeline-jenkinsfiles",
-    message: "Welcome to Project 2 APIs",
+    message: "Welcome to Project 2 API",
   });
 });
 
@@ -15,7 +15,7 @@ app.get("/health", (req, res) => {
   res.status(200).json({
     status: "ok",
     project: "project-2-pipeline-jenkinsfile",
-    message: "Service is healthyy",
+    message: "Service is healthy",
   });
 });
 
